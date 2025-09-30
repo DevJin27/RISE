d5dafb177674e3d3f877cab0bacf5ec701bff17e
DevJin27 <devjindal2317@gmail.com>
2025-09-30 15:43:34 +0530

    added landing page and empty auth pages

---
+++ client/rise-dsa/.next/static/chunks/src_app_(landingpage)_page_jsx_445a3400._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_ac56750c._.js",
+  "static/chunks/src_app_(landingpage)_page_jsx_3e7a2cf5._.js"
+],
+    source: "dynamic"
+});
