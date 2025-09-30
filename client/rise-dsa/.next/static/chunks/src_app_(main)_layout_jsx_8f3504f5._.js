4b7ce430a4dc69bd31eb17b4aac2026eed3174b8
DevJin27 <devjindal2317@gmail.com>
2025-10-01 01:28:52 +0530

    idk

--- client/rise-dsa/.next/static/chunks/src_app_(main)_layout_jsx_8f3504f5._.js
+++ client/rise-dsa/.next/static/chunks/src_app_(main)_layout_jsx_8f3504f5._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_4d62c5e0._.js",
+  "static/chunks/node_modules_3be394c3._.js",
   "static/chunks/src_c7e84d03._.js"
 ],
     source: "dynamic"
